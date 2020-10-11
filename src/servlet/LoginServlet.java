aceab6aa4deeb1b22f2f239af7f7d09af697ff81
ivanfsilva <ivanfs27@gmail.com>
2020-10-11 11:00:16 -0300

    evitando consumir acesso ao banco se usuario nao informar login e senha

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -32,13 +32,22 @@ public class LoginServlet extends HttpServlet {
 			String login = request.getParameter("login");
 			String senha = request.getParameter("senha");
 			
-			if (daoLogin.validarLogin(login, senha)) {
-				RequestDispatcher dispatcher = request.getRequestDispatcher("acessoliberado.jsp");
-				dispatcher.forward(request, response);
+			if (login != null && !login.isEmpty() && 
+					senha != null && !senha.isEmpty()) {
+				
+				if (daoLogin.validarLogin(login, senha)) {
+					RequestDispatcher dispatcher = request.getRequestDispatcher("acessoliberado.jsp");
+					dispatcher.forward(request, response);
+				} else {
+					RequestDispatcher dispatcher = request.getRequestDispatcher("acessonegado.jsp");
+					dispatcher.forward(request, response);
+				}
 			} else {
 				RequestDispatcher dispatcher = request.getRequestDispatcher("acessonegado.jsp");
 				dispatcher.forward(request, response);
 			}
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
